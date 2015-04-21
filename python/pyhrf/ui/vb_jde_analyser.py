[ci skip] [tests] add new clean file for vem_bold and fix beta computation

python/pyhrf/ui/vb_jde_analyser.py
@@ -8,6 +8,7 @@ from pyhrf.ui.analyser_ui import FMRIAnalyser
 from pyhrf.ndarray import xndarray
 from pyhrf.vbjde.vem_tools import roc_curve
 from pyhrf.vbjde.vem_bold import Main_vbjde_Extension, Main_vbjde_Extension_stable, Main_vbjde_Python
+from pyhrf.vbjde.vem_bold_new import jde_vem_bold
 from pyhrf.vbjde.vem_bold_constrained import Main_vbjde_Extension_constrained, Main_vbjde_Python_constrained
 from scipy.linalg import norm
 from pyhrf.tools._io import read_volume
@@ -172,11 +173,12 @@ class JDEVEMAnalyser(JDEAnalyser):
             if not self.constrained:
                 logger.info("fast VEM with drift estimation")
 
-                NbIter, nrls, estimated_hrf, \
-                labels, noiseVar, mu_k, sigma_k, \
-                Beta, L, PL, CONTRAST, CONTRASTVAR, \
-                cA,cH,cZ,cAH,cTime,cTimeMean, Sigma_nrls, \
-                StimuIndSignal = Main_vbjde_Extension_stable(graph,data,Onsets, \
+                (NbIter, nrls, estimated_hrf,
+                labels, noiseVar, mu_k, sigma_k,
+                Beta, L, PL, CONTRAST, CONTRASTVAR,
+                cA,cH,cZ,cAH,cTime,cTimeMean, Sigma_nrls,
+                #StimuIndSignal = Main_vbjde_Extension_stable(graph,data,Onsets,
+                StimuIndSignal) = jde_vem_bold(graph, data, Onsets,
                                         self.hrfDuration, self.nbClasses,TR,
                                         beta,self.dt,scale,self.estimateSigmaH,
                                         self.sigmaH,self.nItMax, self.nItMin,

python/pyhrf/vbjde/vem_bold.py
@@ -1318,8 +1318,11 @@ def Main_vbjde_Extension_stable(graph, Y, Onsets, Thrf, K, TR, beta, dt,
         if estimateBeta:
             logger.info("estimating beta")
             for m in xrange(0, M):
-                Beta[m] = UtilsC.maximization_beta(beta, q_Z[m, :, :].astype(np.float64), Z_tilde[m, :, :].astype(
-                    np.float64), J, K, neighboursIndexes.astype(np.int32), gamma, maxNeighbours, MaxItGrad, gradientStep)
+                Beta[m] = UtilsC.maximization_beta(
+                    beta, q_Z[m, :, :].astype(np.float64),
+                    Z_tilde[m, :, :].astype( np.float64), J, K,
+                    neighboursIndexes.astype(np.int32), gamma, maxNeighbours,
+                    MaxItGrad, gradientStep)
             logger.info("End estimating beta")
             logger.info(Beta)
         logger.info("M sigma noise step ...")

python/pyhrf/vbjde/vem_tools.py
@@ -35,12 +35,35 @@ logger = logging.getLogger(__name__)
 eps = 1e-6
 
 
-def mult(v1, v2):
+def mult_old(v1, v2):
     matrix = np.zeros((len(v1), len(v2)), dtype=float)
     for i in xrange(len(v1)):
         for j in xrange(len(v2)):
             matrix[i, j] += v1[i] * v2[j]
     return matrix
+
+
+def mult(v1, v2):
+    """Multiply two vectors.
+
+    The first vector is made vertical and the second one horizontal. The result
+    will be a matrix of size len(v1), len(v2).
+
+    Parameters
+    ----------
+    v1 : ndarray
+        unidimensional
+    v2 : ndarray
+        unidimensional
+
+    Returns
+    -------
+    x : ndarray, shape (len(v1), len(v2))
+    """
+
+    v1 = v1.reshape(len(v1), 1)
+    v2 = v2.reshape(1, len(v2))
+    return v1.dot(v2)
 
 
 def maximum(a):