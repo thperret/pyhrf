[ci skip] fix AH criteria computation

python/pyhrf/vbjde/vem_bold_new.py
@@ -342,14 +342,18 @@ def jde_vem_bold(graph, bold_data, onsets, hrf_duration, nb_classes, tr, beta,
         DIFF[np.where((DIFF < 1e-50) & (DIFF > 0.0))] = 0.0
         # To avoid numerical problems
         DIFF[np.where((DIFF > -1e-50) & (DIFF < 0.0))] = 0.0
-        if np.linalg.norm(np.reshape(AH1, (nb_conditions * nb_voxels * hrf_len))) > 0:
-            Crit_AH = (
-                np.linalg.norm(DIFF) /
-                (np.linalg.norm(np.reshape(
-                    AH1, (nb_conditions * nb_voxels * hrf_len))) + eps)) ** 2
-        else:
-            # TODO: norm shouldn't be 0
-            Crit_AH = None
+        # if np.linalg.norm(np.reshape(AH1, (nb_conditions * nb_voxels * hrf_len))) > 0:
+            # Crit_AH = (
+                # np.linalg.norm(DIFF) /
+                # (np.linalg.norm(np.reshape(
+                    # AH1, (nb_conditions * nb_voxels * hrf_len))) + eps)) ** 2
+        # else:
+            # # TODO: norm shouldn't be 0
+            # Crit_AH = None
+        Crit_AH = (
+            np.linalg.norm(DIFF) /
+            (np.linalg.norm(np.reshape(
+                AH1, (nb_conditions * nb_voxels * hrf_len))) + eps)) ** 2
         logger.info("Convergence criteria: %f (Threshold = %f)",
                     Crit_AH, thresh)
         cAH += [Crit_AH]